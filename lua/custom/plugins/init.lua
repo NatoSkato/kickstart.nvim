63d171a82557a6176c711ceb91f4c89a2b2b0541
NatoSkato <nathanwebster049@gmail.com>
2025-09-24 13:31:55 +0100

    pdf bug fix

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,7 +7,7 @@ return {
     'lervag/vimtex',
     init = function()
       -- VimTeX configuration goes here, e.g.
-      if vim.fn.has 'win32' then
+      if vim.fn.has 'win32' == 1 then
         vim.cmd [[
         let g:vimtex_view_general_viewer = 'SumatraPDF'
 	let g:vimtex_view_general_options
