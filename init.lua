74e4c4dff8ba9022b349d75d2a11ff8b0165b4c5
NathanW <nathanwebster049@gmail.com>
2025-10-03 00:32:16 +0100

    pretty much just made the begin section thing work in general

--- init.lua
+++ init.lua
@@ -1060,10 +1060,10 @@ imap <silent><expr> <Tab> luasnip#expandable() ? '<Plug>luasnip-expand-snippet' 
 " Jump forward in through tabstops in insert and visual mode with Control-f
 imap <silent><expr> <C-f> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<C-f>'
 smap <silent><expr> <C-f> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<C-f>'
-"The next two lines are a fix for jumping being disabled when nvim cmp is for some reason"
-smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-imap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-""
+""The next two lines are a fix for jumping being disabled when nvim cmp is for some reason"
+"smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+"imap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+"""
 imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 ]]
