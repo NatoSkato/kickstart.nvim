9f16000ae20290534aa34b8d92add6df61b9b159
NathanW <nathanwebster049@gmail.com>
2025-10-11 20:32:02 +0100

    commented custom maps

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -6,12 +6,15 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
+--next and prev occurence of search term
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+--autoindent current paragraph
 vim.keymap.set('n', '=ap', "ma=ap'a")
 vim.keymap.set('n', '<leader>zig', '<cmd>LspRestart<cr>')
 
 -- greatest remap (apparently)
+-- Pastes over line without replacing your yank buffer with the written over text
 vim.keymap.set('x', '<leader>p', '"_dP')
 
 -- next greatest remap ever : asbjornHaland
@@ -25,5 +28,6 @@ vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
+--replace all ocurrences of current word
 vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
